b9e8bbf3f1bca44ebe311afc9841c0534c561759
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-03-07 10:07:43 -0800

    Rebuilt calculator functions

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             ["7","8", "9", "x"],
             ["4","5", "6", "-"],
             ["1","2", "3", "+"],
-            ["0", " . " , "="]
+            ["0", "." , "="]
         ]
     @State private var displayNum: String = "0"
     @State private var num1: String = "0"
@@ -29,25 +29,19 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-            
-                if displayNum <= "7" {
+              
                     Text("\(displayNum)")
                         .font(.system(size: 60))
                         .foregroundColor(.white)
                         .frame(maxWidth: .infinity, alignment: .trailing)
+                        .offset(y: 100)
                         .padding()
                         .bold()
-                } else {
-                    Text("\(displayNum)")
-                        .font(.system(size: 60))
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                        .padding()
-                        .bold()
-                }
+                       
                 
                 
             }
+            .padding()
                 ForEach(numsAndOperations.indices, id: \.self) { num in
                     HStack {
                         ForEach(numsAndOperations[num], id: \.self) { char in
@@ -81,6 +75,10 @@ struct ContentView: View {
                                         num1 = displayNum
                                         displayNum = "0"
                                         oprator = "÷"
+                                        
+                                        if char == "÷" && char == "0"{
+//                                            displayNum -= "0"
+                                        }
                                     } else if char == "=" {
                                         num2 = displayNum
                                         
@@ -131,6 +129,7 @@ struct ContentView: View {
                                             if let displayDouble = Double(displayNum) {
                                                 displayNum = String(displayDouble * 0.01)
                                             }
+                                              
                                             
                                         } else if char == "+/-" {
                                             if let number = Int(displayNum) {
@@ -151,16 +150,17 @@ struct ContentView: View {
                                         .background(Color("Light Gray"))
                                         .clipShape(Circle())
                                 })
+                              
                                 
                             } else if char == "0" {
                                 withAnimation {
                                     Button(action: {
-                                     
+                                         displayNum += "0"
                                     }, label: {
                                         Text("\(char)")
                                             .foregroundColor(.white)
                                             .font(.system(size: 25))
-                                            .frame(width: 130, height: 40)
+                                            .frame(width: 120, height: 40)
                                             .padding()
                                             .background(Color("Dark Gray"))
                                             .cornerRadius(40)
@@ -172,15 +172,30 @@ struct ContentView: View {
                                 
                                 Button(action: {
                                     numsAndOperations[0][0] = "C"
-                                
-                                   
-                                    if displayNum == "0" {
-                                        displayNum = char
-                                    } else {
-                                        
-                                        displayNum += char
-                                      
+                                    
+                                    
+                                    
+                                    if displayNum.count <= 6 {
+                                        if displayNum == "0" {
+                                            displayNum = char
+                                        } else {
+                                            
+                                            displayNum += char
+                                            
+                                            
+                                        }
+                                    } else if displayNum.count <= 9 {
+                                        Text("\(displayNum)")
+                                        //Greather then seven
+                                            .font(displayNum.count > 7 ? .system(size: 40) : .system(size: 60))
+                                            .foregroundColor(.white)
+                                            .frame(maxWidth: .infinity, alignment: .trailing)
+                                            .offset(y: 60)
+                                            .bold()
+                                       
+                                            
                                     }
+                                    
                                     
                                 }, label: {
                                     Text("\(char)")
@@ -199,7 +214,8 @@ struct ContentView: View {
                         }
                             
                     }
-                    .padding()
+                    .offset(y: 100)
+
                 
                     
             }
@@ -210,7 +226,7 @@ struct ContentView: View {
                 .ignoresSafeArea()
         }
      
-        
+       
         
     }
 }
@@ -220,44 +236,110 @@ struct ContentView: View {
         
 }
 
-func add(num1: String, num2: String) -> String {
-    if let number1 = Double(num1), let number2 = Double(num2) {
-        let result = number1 + number2
-        
-        return String(result)
-    } else {
-        return "Invalid Result"
+extension Int {
+
+    private static var commaFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+
+    var delimiter: String {
+            return Int.commaFormatter.string(from: NSNumber(value: self)) ?? ""
+        }
+}
+
+extension Double {
+    private static var commaFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+
+    var delimiter: String {
+        return Double.commaFormatter.string(from: NSNumber(value: self)) ?? ""
     }
+}
+
+func add(num1: String, num2: String) -> String {
+    print("Num1: \(num1.count)")
+    print("Num2: \(num2.count)")
+    if num1.contains(".") || num2.contains(".") {
+        if let number1 = Double(num1), let number2 = Double(num2) {
+            let result = number1 + number2
+            
+           
+            
+            return String(result.delimiter)
+        }
+    } else {
+        if let number1 = Int(num1), let number2 = Int(num2) {
+            let result = number1 + number2
+            
+            return String(result.delimiter)
+        }
+    }
+    
+    return "Error"
 }
 
 func subtract(num1: String, num2: String) -> String {
-    if let number1 = Double(num1), let number2 = Double(num2) {
-        let result = number1 - number2
-        
-        return String(result)
+    if num1.contains(".") || num2.contains(".") {
+        if let number1 = Double(num1), let number2 = Double(num2) {
+            let result = number1 - number2
+            
+            return String(result.delimiter)
+        }
     } else {
-        return "Invalid Result"
+        if let number1 = Int(num1), let number2 = Int(num2) {
+            let result = number1 - number2
+            
+            return String(result.delimiter)
+        }
     }
+    
+    return "ERROR"
+    
+   
 }
 
 func divde(num1: String, num2: String) -> String {
-    if let number1 = Double(num1), let number2 = Double(num2) {
-        let result = number1 / number2
-        
-        return String(result)
+    
+    if num1.contains(".") || num2.contains(".") {
+        if let number1 = Double(num1), let number2 = Double(num2) {
+            let result = number1 / number2
+            
+            return String(result.delimiter)
+        }
+    } else if num1 == "0" || num2 == "0" {
+        return "ERROR"
     } else {
-        return "Invalid Result"
+        if let number1 = Int(num1), let number2 = Int(num2) {
+            let result = number1 / number2
+            
+            return String(result.delimiter)
+        }
     }
+      return "Invalid Result"
+    
 }
 
 func mutiply(num1: String, num2: String) -> String {
-    if let number1 = Double(num1), let number2 = Double(num2) {
-        let result = number1 * number2
-        
-        return String(result)
+   
+    if num1.contains(".") || num2.contains(".") {
+        if let number1 = Double(num1), let number2 = Double(num2) {
+            let result = number1 * number2
+            
+            return String(result.delimiter)
+        }
     } else {
-        return "Invalid Result"
+        if let number1 = Int(num1), let number2 = Int(num2) {
+            let result = number1 * number2
+            
+            return String(result.delimiter)
+        }
     }
+        return "Invalid Result"
 }
 
 
